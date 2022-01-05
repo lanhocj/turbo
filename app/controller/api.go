d4ed85852a2fe3b2799881f0cb1850bfa6302730
Laam Ho <hellolaamho@gmail.com>
2022-01-05 10:41:20 +0800

    Fix f37a881 time out

--- app/controller/api.go
+++ app/controller/api.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/laamho/turbo/app/service/transport"
 	"github.com/laamho/turbo/app/service/transport/client"
 	"github.com/laamho/turbo/common"
 	"github.com/laamho/turbo/common/orm"
 	"log"
+	"regexp"
 	"strings"
 	"sync"
 )
@@ -22,13 +24,21 @@ func deleteNodesByEmail(email string) {
 		go func(node *orm.Node) {
 			lock.Lock()
 			defer lock.Unlock()
-
+			deadline := 0
+			
+		reset:
 			c := client.NewServiceClient(node.NodeAddr, node.NodePort)
 			tag := strings.ToLower(node.NodeTag)
+			deadline = deadline + 1
 
 			// 删除用户
 			if err := transport.RemoveUser(tag, email, c); err != nil {
 				common.Silent(err)
+
+				if ok, _ := regexp.MatchString(`Unavailable`, err.Error()); ok && deadline <= 10 {
+					fmt.Printf("第 %d 次尝试", deadline)
+					goto reset
+				}
 			}
 		}(node)
 	}
@@ -54,22 +64,43 @@ func refreshNodesByUser(user *orm.User) {
 		go func(node *orm.Node, user *orm.User) {
 			lock.Lock()
 			defer lock.Unlock()
+			deadline := 0
 
+		resetDelete:
 			c := client.NewServiceClient(node.NodeAddr, node.NodePort)
 			tag := strings.ToLower(node.NodeTag)
-
+			deadline = deadline + 1
 			// 删除用户
 			if err := transport.RemoveUser(tag, user.Email, c); err != nil {
 				common.Silent(err)
+
+				if ok, _ := regexp.MatchString(`Unavailable`, err.Error()); ok && deadline <= 10 {
+					fmt.Printf("第 %d 次尝试", deadline)
+					goto resetDelete
+				}
 			} else {
 				log.Printf("成功删除：[%s(%s)]->[%s]\n", user.Email, user.Token, node.NodeAddr)
 			}
+
+			if deadline != 0 {
+				deadline = 0
+			}
+
+		reset:
+			c = client.NewServiceClient(node.NodeAddr, node.NodePort)
+			tag = strings.ToLower(node.NodeTag)
+			deadline = deadline + 1
 
 			// 刷新用户
 			if common.Combine(node.ID, nids) && user.Role != orm.LAVEL_USER_BLOCK {
 				if err := transport.AddUser(tag, user.Email, user.Token, 0, c); err != nil {
 					common.Silent(err)
 					log.Printf("用户添加失败：[%s(%s)] error: %s \n", user.Email, node.NodeAddr, err.Error())
+
+					if ok, _ := regexp.MatchString(`Unavailable`, err.Error()); ok && deadline <= 10 {
+						fmt.Printf("第 %d 次尝试", deadline)
+						goto reset
+					}
 				} else {
 					log.Printf("用户添加失败：[%s(%s)]->[%s]\n", user.Email, user.Token, node.NodeAddr)
 				}
